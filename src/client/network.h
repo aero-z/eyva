40741cde52903c03fc63ec4bc127985e7757ba92
ayekat <takeya@bluewin.ch>
2011-11-14 22:59:57 +0100

    Corrected flag definition placing mistake
    * Definition of MSG_NOSIGNAL now at the correct place.

--- src/client/network.h
+++ src/client/network.h
@@ -1,18 +1,6 @@
 #ifndef _NETWORK_H_
 #define _NETWORK_H_
 
-/* Mac OS X does not know the MSG_NOSIGNAL flag; therefore we "link"
- * MSG_NOSIGNAL to SO_NOSIGPIPE.
- * (If there's an error with sending data, the application crashes at send(),
- * rather than returning -1. That's the default behaviour (FSM knows why), so to
- * avoid that, the flag is required.)
- *
- * TODO see if Linux would also get along with SO_NOSIGPIPE
- */
-#ifndef MSG_NOSIGNAL
-#define MSG_NOSIGNAL SO_NOSIGPIPE
-#endif
-
 // Client:
 #include "pipe.h"
 #include "ui/ui.h"
@@ -35,6 +23,18 @@
 // Others:
 #include <cerrno>
 #include <cstring>
+
+/* Mac OS X does not know the MSG_NOSIGNAL flag; therefore we "link"
+ * MSG_NOSIGNAL to SO_NOSIGPIPE.
+ * (If there's an error with sending data, the application crashes at send(),
+ * rather than returning -1. That's the default behaviour (FSM knows why), so to
+ * avoid that, the flag is required.)
+ *
+ * TODO see if Linux would also get along with SO_NOSIGPIPE
+ */
+#ifndef MSG_NOSIGNAL
+#define MSG_NOSIGNAL SO_NOSIGPIPE
+#endif
 
 class
 Network
