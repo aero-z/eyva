8d1a39c1940fec9fa75330f23bfabbb884204a05
ayekat <takeya@bluewin.ch>
2011-11-15 10:01:41 +0100

    Redefined window management.

--- src/client/ui/vars.h
+++ src/client/ui/vars.h
@@ -5,19 +5,43 @@
 #ifndef _VARS_H_
 #define _VARS_H_
 
+/**
+ * Window definition.
+ * The windows are arranged as follows:
+ *
+ * +---------------+---------------+---------------+
+ * |               |               |               |
+ * |               |  NAVIGATION   |               |
+ * |               |               |               |
+ * +---------------+---------------+---------------+
+ * |               |               |               |
+ * |   CHARACTER   |     GAME      |    SOCIAL     |
+ * |               |               |               |
+ * +---------------+---------------+---------------+
+ * |               |               |               |
+ * |               |     MENU      |    OPTIONS    |
+ * |               |               |               |
+ * +---------------+---------------+---------------+
+ * |               |               |               |
+ * |               |     LOGIN     |               |
+ * |               |               |               |
+ * +---------------+---------------+---------------+
+ */
+
 enum WindowName {
-	PLAYGROUND,     //         where you play the game
-	PROMPT,         // [ENTER] prompt (also "fast chat")
-	PROMPT_COMMAND, // [':']   prompt with a preceding ':'
-	INFOBAR,        //         top panel, showing information
-	ACTIONBAR,      //         bottom panel, showing actions and information
 	LOGIN,          //         login window
 	MENU,           // [ESC]   main menu, containing
 	OPTIONS,        //         - options
-	NAVIGATION,     // ['m']   minimap and orientation tools
-	SOCIAL,         // [TAB]   ingame chat, online stats, etc.
+	GAME,           // contains further windows:
+		PLAYGROUND,     //         where you play the game
+		PROMPT,         // [ENTER] prompt (also "fast chat")
+		PROMPT_COMMAND, // [':']   prompt with a preceding ':'
+		INFOBAR,        //         top panel, showing information
+		ACTIONBAR,      //         bottom panel, showing actions and information
+		INVENTORY,      // ['b']   character inventory
 	CHARACTER,      // ['c']   ingame character settings
-	INVENTORY,      // ['b']   character inventory
+	SOCIAL,         // [TAB]   ingame chat, online stats, etc.
+	NAVIGATION,     // ['m']   minimap and orientation tools
 
 	IDENTITY,       // (virtual) currently focused window
 	TERM            // (virtual) indicates program shutdown
